f7c7cce046d2181ed9de806eca54544e46617607
Tanishq Jadhav <tanishqjadhav00@gmail.com>
2025-05-02 17:12:18 +0530

    changes

--- js/index.js
+++ js/index.js
@@ -4,3 +4,25 @@ function toggleMenu() {
     navbar.classList.toggle('active');
     hamburger.classList.toggle('active');
 }
+
+
+const counters = document.querySelectorAll('.count');
+const speed = 200; // lower = faster
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+        const increment = Math.ceil(target / speed);
+
+        if (count < target) {
+            counter.innerText = count + increment;
+            setTimeout(updateCount, 20);
+        } else {
+            counter.innerText = target + '+'; // add the "+" after animation
+        }
+    };
+
+    updateCount();
+});
